35079e10724a1f77aad4e42f8f9bb1f948085532
monstermobster <zakariasribaric@hotmail.com>
2020-02-27 10:49:27 +0100

    robin 3

--- test/unit/helpers.js
+++ test/unit/helpers.js
@@ -11,8 +11,10 @@ const makeDescription = (text, args) => {
 	return values.reduce((prev, v) => prev.replace('{}', v), text)
 }
 
-module.exports.message = send => ({
-	channel: { send },
+module.exports.message = (send = () => {}) => ({
+	channel: {
+		send,
+	},
 })
 
 module.exports.runWith = (cmd, {
